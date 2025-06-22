40fee987eedb7e69c3f15c1a5273192ad8a67f55
ChawinSS <137822600+ChawinSS@users.noreply.github.com>
2025-06-22 12:19:46 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,152 @@
+const quizDataFromFile = [
+  {
+    "question": "bald",
+    "correct": "soon",
+    "choices": [
+      "soon",
+      "never",
+      "always",
+      "already"
+    ]
+  },
+  {
+    "question": "besser",
+    "correct": "better",
+    "choices": [
+      "always",
+      "better",
+      "now",
+      "…"
+    ]
+  },
+  {
+    "question": "dann",
+    "correct": "then (afterwards)",
+    "choices": [
+      "soon",
+      "then (afterwards)",
+      "…",
+      "not"
+    ]
+  },
+  {
+    "question": "gern(e)",
+    "correct": "…",
+    "choices": [
+      "not",
+      "perhaps / may(be)",
+      "often",
+      "…"
+    ]
+  },
+  {
+    "question": "gestern",
+    "correct": "yesterday",
+    "choices": [
+      "now",
+      "yesterday",
+      "perhaps / may(be)",
+      "no"
+    ]
+  },
+  {
+    "question": "heute",
+    "correct": "today",
+    "choices": [
+      "soon",
+      "perhaps / may(be)",
+      "today",
+      "…"
+    ]
+  },
+  {
+    "question": "immer",
+    "correct": "always",
+    "choices": [
+      "always",
+      "perhaps / may(be)",
+      "often",
+      "immediately"
+    ]
+  },
+  {
+    "question": "jetzt",
+    "correct": "now",
+    "choices": [
+      "now",
+      "already",
+      "perhaps / may(be)",
+      "never"
+    ]
+  },
+  {
+    "question": "nicht",
+    "correct": "not",
+    "choices": [
+      "not",
+      "today",
+      "often",
+      "immediately"
+    ]
+  },
+  {
+    "question": "nicht",
+    "correct": "no",
+    "choices": [
+      "not",
+      "no",
+      "often",
+      "…"
+    ]
+  },
+  {
+    "question": "nie",
+    "correct": "never",
+    "choices": [
+      "always",
+      "never",
+      "yesterday",
+      "no"
+    ]
+  },
+  {
+    "question": "oft",
+    "correct": "often",
+    "choices": [
+      "soon",
+      "better",
+      "today",
+      "often"
+    ]
+  },
+  {
+    "question": "schon",
+    "correct": "already",
+    "choices": [
+      "soon",
+      "now",
+      "no",
+      "already"
+    ]
+  },
+  {
+    "question": "sofort",
+    "correct": "immediately",
+    "choices": [
+      "now",
+      "no",
+      "then (afterwards)",
+      "immediately"
+    ]
+  },
+  {
+    "question": "vielleicht",
+    "correct": "perhaps / may(be)",
+    "choices": [
+      "perhaps / may(be)",
+      "no",
+      "now",
+      "never"
+    ]
+  }
+];
